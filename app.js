08fdd5c8f8bb3dacc655013a88c1b4245b7462cb
Erika Joy De La Cruz <erikajoy.odc@gmail.com>
2021-02-16 17:22:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,64 @@
 const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+const phrase = document.querySelector('#phrase ul');
 
 let missed = 0;
 
 const startButton = document.getElementsByClassName('btn__reset')[0];
 
+//hide start overlay
 startButton.addEventListener('click', (e) => {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
 
 let phrases = [
-    ['treehouse techdegree'],
-    ['javascript array'],
-    ['object literal'],
-    ['for loop'],
-    ['conditional statement']
-]
+    'treehouse techdegree',
+    'javascript array',
+    'object literal',
+    'for loop',
+    'conditional statement'
+];
+
+/*get random phrase
+split phrase into array of characters
+return array of characters*/
 function getRandomPhraseAsArray(arr) {
-
-
+    const rando =  arr[Math.floor(Math.random() * arr.length)];
+    return rando.split('')
 };
+
+getRandomPhraseAsArray(phrases);
+
+//loops through an array of characters
+function addPhrasetoDisplay(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement('li'); //create li element
+        //get character inside
+        li.textContent = arr[i] //text content of li is arr[i]
+        phrase.appendChild(li); //append li to phrase which is `#phrase ul`
+
+        if (arr[i] === ' ') {
+            li.className = 'space'; 
+        } else {
+            li.className = 'letter';
+        }
+    }
+};
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhrasetoDisplay(phraseArray);
+
+function checkLetter(btn) {
+    let letter = document.getElementsByClassName('letter');
+    console.log(letter);
+};
+    /* get all elements with class letter 
+    loop  over letters 
+    check if they match the letter in the button the player has chosen
+    if match
+        add "show" class to list item containing the letter 
+        store matching letter inside variable
+        return letter
+    else
+        return null
+        */
